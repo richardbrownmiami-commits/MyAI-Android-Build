c88195c0135be59be369fac8b78308b670f233e5
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-09-23 00:37:46 +0530

    Complete cognitive representation metadata and bidirectional bridge

--- brain/atomspace/core/atom.cpp
+++ brain/atomspace/core/atom.cpp
@@ -1,16 +1,16 @@
 #include "brain/atomspace/atom.h"
 #include <sstream>
-
+#include <utility>
 namespace brain::atomspace {
-Atom::Atom(AtomId id, AtomType type, TruthValue truth) : id_(id), type_(type), truth_(truth) {}
-Node::Node(AtomId id, AtomType type, std::string name, TruthValue truth)
-    : Atom(id, type, truth), name_(std::move(name)) {}
+Atom::Atom(AtomId id, AtomType type, TruthValue truth, AtomMetadata metadata)
+    : id_(id), type_(type), truth_(truth), metadata_(std::move(metadata)) {}
+Node::Node(AtomId id, AtomType type, std::string name, TruthValue truth, AtomMetadata metadata)
+    : Atom(id, type, truth, std::move(metadata)), name_(std::move(name)) {}
 std::string Node::key() const { return std::to_string(static_cast<TypeId>(type())) + ":N:" + name_; }
-Link::Link(AtomId id, AtomType type, std::vector<AtomId> outgoing, TruthValue truth)
-    : Atom(id, type, truth), outgoing_(std::move(outgoing)) {}
+Link::Link(AtomId id, AtomType type, std::vector<AtomId> outgoing, TruthValue truth, AtomMetadata metadata)
+    : Atom(id, type, truth, std::move(metadata)), outgoing_(std::move(outgoing)) {}
 std::string Link::key() const {
-    std::ostringstream s;
-    s << static_cast<TypeId>(type()) << ":L:";
+    std::ostringstream s; s << static_cast<TypeId>(type()) << ":L:";
     for (AtomId id : outgoing_) s << id << ',';
     return s.str();
 }
