6ba5d1b041373426e0d3434e223084472f7f5323
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-09-22 23:40:46 +0530

    Connect brain core to ONA NAR API

---
+++ brain/nars/core/ona_bridge.cpp
@@ -0,0 +1,20 @@
+#include "brain/nars/ona_bridge.h"
+
+extern "C" {
+#include "NAR.h"
+}
+
+namespace brain::nars {
+void init() {
+    NAR_INIT();
+}
+
+void cycles(int count) {
+    if (count > 0) NAR_Cycles(count);
+}
+
+void add_narsese(const char* sentence) {
+    if (sentence == nullptr) return;
+    NAR_AddInputNarsese(const_cast<char*>(sentence));
+}
+}
