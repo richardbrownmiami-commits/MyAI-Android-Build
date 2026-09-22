5335c733a6cecaa4beca6044849a7a8617783100
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-09-22 23:41:07 +0530

    Expose brain orchestrator through Android JNI

--- app/src/main/cpp/brain_jni.cpp
+++ app/src/main/cpp/brain_jni.cpp
@@ -1,17 +1,33 @@
 #include <jni.h>
 #include "brain/brain_core.h"
-#include "brain/atomspace/atomspace.h"
-#include "brain/representation/bridge.h"
-static brain::atomspace::AtomSpace g_space;
+#include "brain/brain/brain_orchestrator.h"
+
+static brain::BrainOrchestrator g_brain;
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myai_BrainNative_version(JNIEnv* env, jclass) {
     return env->NewStringUTF(brain::version());
 }
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myai_BrainNative_addInheritance(JNIEnv* env, jclass, jstring a, jstring b) {
-    const char* ca=env->GetStringUTFChars(a,nullptr), *cb=env->GetStringUTFChars(b,nullptr);
-    auto id=brain::representation::inheritance(g_space,ca,cb);
-    auto s=brain::representation::atom_to_narsese(g_space,id);
-    env->ReleaseStringUTFChars(a,ca); env->ReleaseStringUTFChars(b,cb);
-    return env->NewStringUTF(s.c_str());
+    const char* ca = env->GetStringUTFChars(a, nullptr);
+    const char* cb = env->GetStringUTFChars(b, nullptr);
+    const std::string result = g_brain.remember_inheritance(ca, cb);
+    env->ReleaseStringUTFChars(a, ca);
+    env->ReleaseStringUTFChars(b, cb);
+    return env->NewStringUTF(result.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myai_BrainNative_reason(JNIEnv* env, jclass, jstring narsese, jint cycles) {
+    const char* input = env->GetStringUTFChars(narsese, nullptr);
+    const std::string result = g_brain.reason(input, static_cast<int>(cycles));
+    env->ReleaseStringUTFChars(narsese, input);
+    return env->NewStringUTF(result.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_myai_BrainNative_atomCount(JNIEnv*, jclass) {
+    return static_cast<jint>(g_brain.atom_count());
 }
