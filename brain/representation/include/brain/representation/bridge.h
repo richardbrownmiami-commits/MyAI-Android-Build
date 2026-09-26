7453a65d228907d1151645c65e6f662c464457ea
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-09-26 11:34:15 +0530

    Add explicit negation representation

--- brain/representation/include/brain/representation/bridge.h
+++ brain/representation/include/brain/representation/bridge.h
@@ -6,6 +6,7 @@ std::string atom_to_narsese(const atomspace::AtomSpace&, atomspace::AtomId);
 atomspace::AtomId inheritance(atomspace::AtomSpace&, const std::string&, const std::string&, double frequency=1.0, double confidence=0.9, const atomspace::AtomMetadata& metadata = {});
 atomspace::AtomId similarity(atomspace::AtomSpace&, const std::string&, const std::string&, const atomspace::AtomMetadata& metadata = {});
 atomspace::AtomId implication(atomspace::AtomSpace&, const std::string&, const std::string&, const atomspace::AtomMetadata& metadata = {});
+atomspace::AtomId negation(atomspace::AtomSpace&, const std::string&, const std::string&, const atomspace::AtomMetadata& metadata = {});
 bool ingest_narsese(atomspace::AtomSpace&, const std::string&, atomspace::AtomId* created = nullptr);
 bool is_explicit_contradiction(const atomspace::AtomSpace&, atomspace::AtomId);
 }
